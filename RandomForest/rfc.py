ff190b81a3db71a26ce20e948446bdfb5dc6a083
youngerm <1505340824@qq.com>
2021-06-23 01:58:27 +0800

    delete "叠品别墅"

---
+++ RandomForest/rfc.py
@@ -0,0 +1,61 @@
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+import pandas as pd
+
+
+def createData():
+    # le = preprocessing.LabelEncoder()
+    house_data = pd.read_csv("sumData.csv")
+    data = house_data.iloc[:, 1:]
+    print("data:\n", data[:5])
+    columns = ['Region', 'District', 'Layout', 'Direction', 'Renovation', ] #, 'Garden'
+    for i in columns:
+        print(data[i])
+        le = preprocessing.LabelEncoder()
+        le.fit(data[i])
+        data[i + "Num"] = le.transform(data[i])
+
+    data["ElevatorNum"] = data["Elevator"]
+    data["YearNum"] = data["Year"]
+    data["SizeNum"] = data["Size"]
+    data["FloorNum"] = data["Floor"]
+    print("data:\n", data[:5])
+    data = pd.DataFrame(data)
+    data.to_csv("numTrainData.csv")
+
+    ######################################################
+    target = {}
+    target['Price'] = house_data['Price']
+    target = pd.DataFrame(target)
+    print("target:\n", target[:5])
+    target.to_csv("numTrainTarget.csv")
+
+
+createData()
+
+data = pd.read_csv("numTrainData.csv")
+data = data.iloc[:, 12:].values
+target = pd.read_csv("numTrainTarget.csv")
+target = target["Price"]
+
+
+# 拆分训练集和测试集
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(data, target, test_size=0.3)
+print(Xtrain[:5])
+print(Ytrain[:5])
+
+clf = DecisionTreeRegressor(random_state=30)
+clf.fit(Xtrain, Ytrain)
+score_c = clf.score(Xtest, Ytest)
+print("single Tree:{}".format(score_c))
+
+
+rfc = RandomForestRegressor(random_state=30,oob_score=True)
+rfc.fit(Xtrain, Ytrain)
+print("Random Forest oob_scoreTrain:{}".format(rfc.oob_score_))
+score_r = rfc.score(Xtest, Ytest)
+print("Random Forest oob_scoreTest:{}".format(rfc.oob_score_))
+print("Random Forest:{}".format(score_r))
+
--- processdata/mergeData.py
+++ processdata/mergeData.py
@@ -12,7 +12,7 @@ from sklearn import preprocessing
 columns = ['Region', 'District', 'Garden', 'Layout', 'Floor', 'Year', 'Size', 'Direction', 'Renovation',
            'Price', 'Elevator']
 dataOrigin = pd.read_csv("不含空值数据文档.csv")
-dataOrigin = pd.DataFrame(dataOrigin,columns=columns)
+dataOrigin = pd.DataFrame(dataOrigin, columns=columns)
 le = preprocessing.LabelEncoder()
 
 le.fit(dataOrigin['Elevator'])
@@ -20,14 +20,14 @@ le.fit(dataOrigin['Elevator'])
 dataOrigin['Elevator'] = le.transform(dataOrigin['Elevator'])
 
 dataFill = pd.read_csv("predNonElevator.csv")
-dataFill = pd.DataFrame(dataFill.iloc[:,1:])
+dataFill = pd.DataFrame(dataFill.iloc[:, 1:])
 
 dataFill = pd.DataFrame(dataFill)
 print(dataFill)
 dataOrigin = pd.DataFrame(dataOrigin)
 print(dataOrigin)
 
-sumData = pd.concat([dataFill,dataOrigin])
-sumData = pd.DataFrame(sumData.iloc[:,1:])
-sumData.to_csv("sumData.csv",index=False)
+sumData = pd.concat([dataFill, dataOrigin])
+sumData = pd.DataFrame(sumData)
+sumData.to_csv("sumData.csv")
 print(sumData)
